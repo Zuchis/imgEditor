73f79e2f63b7fdac9fa7836ca9131f55eaa54915
Zuchis <liquimaster@gmail.com>
2015-08-17 19:50:26 -0300

    Implementing the "open file" function

--- GUI/masterMind.py
+++ GUI/masterMind.py
@@ -2,7 +2,11 @@ import sys
 from PyQt4 import *
 from imageEditor import *
 
-class gui(QtGui.QMainWindow, Ui_MainWindow):
+class gui(QtGui.QMainWindow, Ui_MainWindow,QtGui.QDialog):
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self)
         self.setupUi(self)
+        QtCore.QObject.connect(self.actionAbrir, QtCore.SIGNAL(("activated()")), self.openFile)
+        
+    def openFile(self):
+        fileName = QtGui.QFileDialog.getOpenFileName(self, "Abrir Imagem")
